9b1b9c51b4abcb138cdc85a03ab0a57223c78e7c
Zhanna <kezhanna.03@mail.ru>
2026-01-01 17:05:32 +0500

    task 3

--- buildLibraryManager.js
+++ buildLibraryManager.js
@@ -5,4 +5,11 @@ const library = [
     about: "A book on how to plan ahead",
     pages: 320,
   },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    about:
+      "A practical book about discarding bad habits and building good ones",
+    pages: 320,
+  },
 ];
