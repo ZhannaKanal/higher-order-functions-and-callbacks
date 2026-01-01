1f0d3315e4a9df27b68f9443e3826de921129805
Zhanna <kezhanna.03@mail.ru>
2026-01-02 01:59:09 +0500

    task 6

--- bookOrganizer.js
+++ bookOrganizer.js
@@ -1,21 +1,23 @@
 const books = [
   {
-    title: 'First',
-    authorName: 'Me',
-    releaseYear: 2010
+    title: "First",
+    authorName: "Me",
+    releaseYear: 2010,
   },
   {
-    title: 'Second',
-    authorName: 'You',
-    releaseYear: 2010
+    title: "Second",
+    authorName: "You",
+    releaseYear: 2010,
   },
   {
-    title: 'First',
-    authorName: 'He',
-    releaseYear: 2010
+    title: "First",
+    authorName: "He",
+    releaseYear: 2010,
   },
-]
+];
 const sortByYear = (book1, book2) => {
   const difference = book1.releaseYear - book2.releaseYear;
-
-}
+  if (difference >= 1) {
+    return 1;
+  }
+};
