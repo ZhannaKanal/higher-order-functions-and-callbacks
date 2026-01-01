a1d21c98dc8cdee6b981dcf67b0975601410b691
Zhanna <kezhanna.03@mail.ru>
2026-01-01 17:04:47 +0500

    task 2

--- buildLibraryManager.js
+++ buildLibraryManager.js
@@ -1,1 +1,8 @@
-const library = [];
+const library = [
+  {
+    title: "Your Next Five Moves: Master the Art of Business Strategy",
+    author: "Patrick Bet-David and Greg Dinkin",
+    about: "A book on how to plan ahead",
+    pages: 320,
+  },
+];
