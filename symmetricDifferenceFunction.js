5200574cb68031433f5a2dd3529aede788d223bc
Zhanna <kezhanna.03@mail.ru>
2026-01-02 16:20:09 +0500

    task 6

--- symmetricDifferenceFunction.js
+++ symmetricDifferenceFunction.js
@@ -2,7 +2,7 @@ const diffArray = (array1, array2) => {
   const newArr = [];
   for (let i = 0; i < array1.length; i++) {
     if (!array2.includes(array1[i])) {
-      
+      newArr.push(array1[i]);
     }
   }
 };
