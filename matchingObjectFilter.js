ea9b6f2be26046eb576bab3e41c04282dbfa7c9c
Zhanna <kezhanna.03@mail.ru>
2026-01-02 20:19:27 +0500

    task 5

--- matchingObjectFilter.js
+++ matchingObjectFilter.js
@@ -1,6 +1,6 @@
 const whatIsInAName = (array, sourceObject) => {
-    const keys = Object.keys(sourceObject);
-    return array.filter(x=>keys.every(y=>x[y]===sourceObject[y]))
+  const keys = Object.keys(sourceObject);
+  return array.filter((x) => keys.every((y) => x[y] === sourceObject[y]));
 };
 console.log(
   whatIsInAName(
@@ -10,5 +10,11 @@ console.log(
       { first: "Tybalt", last: "Capulet" },
     ],
     { last: "Capulet" }
+  )
+);
+console.log(
+  whatIsInAName(
+    [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+    { apple: 1, cookie: 2 }
   )
 );
