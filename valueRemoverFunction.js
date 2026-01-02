81d22ac1b67f4be98fe52117c3a3fcd501ff2897
Zhanna <kezhanna.03@mail.ru>
2026-01-02 17:02:10 +0500

    task 4

--- valueRemoverFunction.js
+++ valueRemoverFunction.js
@@ -2,3 +2,4 @@ const destroyer = (array, ...args) => {
   const filteredArray1 = array.filter((x) => !args.includes(x));
   return filteredArray1;
 };
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3))
