1b8a77a107cd261d4c275a51852fd282f317e9dc
Zhanna <kezhanna.03@mail.ru>
2026-01-02 15:57:14 +0500

    task 8

--- sortedIndexFinder.js
+++ sortedIndexFinder.js
@@ -2,7 +2,8 @@ const getIndexToIns = (array, number) => {
   const sorted = array.sort((a, b) => a - b);
   const index = sorted.findIndex((result) => result >= number);
   return index === -1 ? sorted.length : index;
-}
-console.log(getIndexToIns([], 5))
-console.log(getIndexToIns([5, 3, 20, 3], 5))
-console.log(getIndexToIns([3, 10, 5], 11))
+};
+console.log(getIndexToIns([], 5));
+console.log(getIndexToIns([5, 3, 20, 3], 5));
+console.log(getIndexToIns([3, 10, 5], 11));
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
