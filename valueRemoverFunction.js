835cea318a7a3c377caa858f309e1883fce64383
Zhanna <kezhanna.03@mail.ru>
2026-01-02 17:04:04 +0500

    task 7

--- valueRemoverFunction.js
+++ valueRemoverFunction.js
@@ -5,3 +5,5 @@ const destroyer = (array, ...args) => {
   return result;
 };
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(destroyer( ["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan" ));
+
