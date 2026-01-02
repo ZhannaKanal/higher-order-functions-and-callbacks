1133194ad2e8f6eba8aa06b408d7a6f826ef8ed9
Zhanna <kezhanna.03@mail.ru>
2026-01-02 16:19:51 +0500

    task 4

--- symmetricDifferenceFunction.js
+++ symmetricDifferenceFunction.js
@@ -1,4 +1,12 @@
 const diffArray = (array1, array2) => {
-  const newArr = []
-}
-console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))
+  const newArr = [];
+  for (let i = 0; i < array1.length; i++) {
+    
+  }
+};
+console.log(
+  diffArray(
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+);
