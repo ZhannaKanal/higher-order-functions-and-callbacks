3f4b3262dfd2a8daead9063ca1be7d088cd00de4
Zhanna <kezhanna.03@mail.ru>
2026-01-03 23:23:51 +0500

    task 5

--- primeNumberSumCalculator.js
+++ primeNumberSumCalculator.js
@@ -1,6 +1,9 @@
 const sumPrimes = (number) => {
   const isPrime = (number) => {
     if (number < 2) return false;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      
+    }
   };
 };
-console.log();
+console.log()
