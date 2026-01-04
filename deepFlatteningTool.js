6f43a56bfda130d6347b2cd21f55b13317880b99
Zhanna <kezhanna.03@mail.ru>
2026-01-04 23:22:47 +0500

    task 14

--- deepFlatteningTool.js
+++ deepFlatteningTool.js
@@ -9,5 +9,6 @@ const steamrollArray = (array) => {
   }, []);
 };
 console.log(steamrollArray([1, {}, [3, [[4]]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]));
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
 console.log(steamrollArray([[["a"]], [["b"]]]));
