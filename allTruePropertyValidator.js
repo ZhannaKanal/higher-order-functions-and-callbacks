ea377bba546686597229ccae8d3f8251c790d3de
Zhanna <kezhanna.03@mail.ru>
2026-01-04 23:41:28 +0500

    task 18

--- allTruePropertyValidator.js
+++ allTruePropertyValidator.js
@@ -17,3 +17,19 @@ console.log(
     "isBot"
   )
 );
+console.log(
+  truthCheck(
+    [
+      {
+        id: 1,
+        data: { url: "https://freecodecamp.org", name: "freeCodeCamp" },
+      },
+      {
+        id: 2,
+        data: { url: "https://coderadio.freecodecamp.org/", name: "CodeRadio" },
+      },
+      { id: null, data: {} },
+    ],
+    "data"
+  )
+);
