a199ba064e7224bcb03bfe25ec8ae557e415fb88
Zhanna <kezhanna.03@mail.ru>
2026-01-04 23:33:19 +0500

    task 8

--- allTruePropertyValidator.js
+++ allTruePropertyValidator.js
@@ -7,11 +7,13 @@ function truthCheck(collection, pre) {
   return result;
 }
 
-truthCheck(
-  [
-    { name: "Quincy", role: "Founder", isBot: false },
-    { name: "Naomi", role: "", isBot: false },
-    { name: "Camperbot", role: "Bot", isBot: true },
-  ],
-  "isBot"
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "isBot"
+  )
 );
